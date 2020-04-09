eff51c8fa8ba13e929aa5d576894ff02c492d254
Hakan Yildiz <hakan@re-coded.com>
2020-04-09 12:32:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 let divide = function(){return 2000/100}
 
-let square = num => square**num;
+let square = num => {
 
+  for(let i = 0; i<num.length; i++){
+    return  square*= square;
+  }
+}
 let add = (a,b) => a+b; 
